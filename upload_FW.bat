5a66d65b47a3d7ddf220c4243a16a9bdfe9735c0
DIY Guy Dapeng <742305060@qq.com>
2021-06-09 09:19:30 +0800

    Update upload_FW.bat

--- upload_FW.bat
+++ upload_FW.bat
@@ -1,6 +1,11 @@
-
+rem 修改等号后的串口号，与板卡的串口号对应
 set PORT=4
+
+rem 修改等号后文件名，即待升级的固件
 set FW=dapeng_slider_RF.ino.hex
 
-bin\avrdude -Cbin\avrdude.conf -v  -patmega328pb -carduino -PCOM%PORT% -b115200 -D "-Uflash:w:%FW%:i"
+rem 修改等号后的芯片型号，tmega328pb 或 tmega328p，目前用的都是328pb所以不用改，以后可能会用328p，固件通用但芯片选对型号固件才写得进去。
+set mcu=atmega328pb
+
+bin\avrdude -Cbin\avrdude.conf -v  -p%mcu% -carduino -PCOM%PORT% -b115200 -D "-Uflash:w:%FW%:i"
 pause
